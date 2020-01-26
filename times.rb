9832f7bb5698e368de3f9376b9ec59b7a9ed7f5d
james Johnson <2013matthew2013@gmail.com>
2020-01-26 14:41:39 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -7,7 +7,7 @@ def times do
   puts "I am doing the dishes left by my former friends."
 end
 
-jewels_in_bag = 100
+def jewels_in_bag = 100
 
 3.times do 
   puts "Hiding 10 stolen jewels."
@@ -26,6 +26,7 @@ jewels_in_bag = 100
   jewels_in_bag = jewels_in_bag - 10
   puts "Now there are only #{jewels_in_bag} jewels left to hide!"
 end
+
 # => 3 (return value)
 
 puts "We have #{jewels_in_bag} jewels still to hide!"
